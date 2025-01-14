3a1dade69355205e697d2b3c2dd1dc05222888be
Subhaan Elburz <sme5522@mavs.uta.edu>
2025-01-13 20:52:11 -0600

    added explanation

--- leetcode/easy/leetcode2239.cpp
+++ leetcode/easy/leetcode2239.cpp
@@ -3,6 +3,13 @@
 
 	Given an integer array nums of size n, return the number with the value closest to 0 in nums. 
 	If there are multiple answers, return the number with the largest value.
+
+	Explanation: First off, to find the closest number, you first set the closest number to the first 
+	value in the array/vector. Then, you iterate through the vector, and if a number's absolute value
+	is less than the current closest value, it will set the closest value to the lower number. The 
+	other conditional checks that if the closest value is the same as the current value AND if the 
+	current value is greater than the closest value, it will set the closest value to the greater 
+	value between the two. After the loop, the function returns the closest integer.
 */
 
 #include <vector>
